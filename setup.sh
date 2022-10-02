0e8b20213bd2bb40c12696b9ac383401c8d842a8
gh <gh6141@gmail.com>
2022-10-02 13:01:29 +0900

    'up'

--- setup.sh
+++ setup.sh
@@ -11,13 +11,12 @@ read tmp
 tpath='/var/lib/tomcat7'
 webinf='/var/lib/tomcat7/webapps/SGW/WEB-INF'
 chmod -R 755 SGW
-cp -R ./SGW $tpath/webapps/
+cp SGW.war $tpath/webapps/
 chmod -R 777 $tpath
 #/var/lib/tomcat7/conf/web.xml  enable xlsx,docx  session_timeout>3minutes********************
 cp excel_komoku_linux.txt $webinf/excel_komoku.txt
 cp excel_path_linux.txt $webinf/excel_path.txt
 cp opath_linux.txt $webinf/opath.txt
-cp opath2_win.txt $webinf/opath2.txt
 cp web_linux.xml $webinf/web.xml
 #web.xml Mysql ID,PW input***********************************
 echo -n "Mysql root password(a****9):"
@@ -25,10 +24,9 @@ read pwd
 sed -i -e "s/<pass>/$pwd/g" $webinf/web.xml
 chmod -R 755 $tpath
 #mysql setup*****************************************
-systemctl restart mysql
-mysql -u root -p$pwd -e "create database skt;"
-mysql -u root -p$pwd skt < skt.sql
-cp my.cnf /etc/mysql
+#systemctl restart mysql
+#mysql -u root -p$pwd -e "create database skt;"
+#mysql -u root -p$pwd skt < skt.sql  #‚±‚Ì‚Ö‚ñ‚ÍŽè“®‚Ås‚¤
 #yotei.xlsx sample copy
 mkdir /tenpu
 chmod 777 /tenpu
@@ -39,8 +37,5 @@ cp tenpu.xml $tpath/conf/Catalina/localhost
 cp mylogohelp.htm /tenpu
 chmod 777 /tenpu/mylogohelp.htm
 chmod 777 $webinf/*.txt
-chmod 777 /etc/mysql/my.cnf
-cp my.cnf /etc/mysql/my.cnf
-chmod 644 /etc/mysql/my.cnf
 systemctl restart tomcat7
 systemctl restart mysql
